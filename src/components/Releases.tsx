692a2f2d9fb22c9b14d3f085f91011e78aab3d9b
medentem <info@medentem.com>
2024-10-22 10:08:40 -0500

    wiring up releases

--- src/components/Releases.tsx
+++ src/components/Releases.tsx
@@ -1,69 +1,51 @@
+import { useFirmwareStore } from "src/stores/firmwareStore";
 import MeshtasticIcon from "./MeshtasticIcon";
 import { FolderPlusIcon } from "@heroicons/react/24/solid";
-
-const releases = [
-  {
-    id: 1,
-    content: "2.5.5.e182ae7",
-    href: "#",
-    date: "Sep 20",
-    datetime: "2020-09-20",
-    type: "alpha",
-    icon: MeshtasticIcon,
-    selected: false,
-    isLatest: false,
-  },
-  {
-    id: 2,
-    content: "2.5.3.a70d5ee",
-    href: "#",
-    date: "Sep 22",
-    datetime: "2020-09-22",
-    type: "alpha",
-    icon: MeshtasticIcon,
-    selected: false,
-    isLatest: false,
-  },
-  {
-    id: 3,
-    content: "2.5.2.771cb52",
-    href: "#",
-    date: "Sep 28",
-    datetime: "2020-09-28",
-    type: "alpha",
-    icon: MeshtasticIcon,
-    selected: false,
-    isLatest: false,
-  },
-  {
-    id: 4,
-    content: "2.5.4.8d288d5",
-    href: "#",
-    date: "Sep 30",
-    datetime: "2020-09-30",
-    type: "beta",
-    icon: MeshtasticIcon,
-    selected: true,
-    isLatest: true,
-  },
-  {
-    id: 5,
-    content: "2.4.2.5b45303",
-    href: "#",
-    date: "Oct 4",
-    datetime: "2020-10-04",
-    type: "beta",
-    icon: MeshtasticIcon,
-    selected: false,
-    isLatest: false,
-  },
-];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Releases() {
+  const stableFirmwareReleases = useFirmwareStore((state) => state.stable);
+  const alphaFirmwareReleases = useFirmwareStore((state) => state.alpha);
+  const previewFirmwareReleases = useFirmwareStore((state) => state.previews);
+  const firmwares = [
+    ...previewFirmwareReleases.map((x) => {
+      return {
+        ...x,
+        classNames: "bg-red-500",
+        type: "preview",
+        selected: false,
+        isLatest: false,
+      };
+    }),
+    ...alphaFirmwareReleases.map((x) => {
+      return {
+        ...x,
+        classNames: "bg-orange-500",
+        type: "alpha",
+        selected: false,
+        isLatest: false,
+      };
+    }),
+    {
+      ...stableFirmwareReleases[0],
+      classNames: "bg-meshtastic-green",
+      type: "stable",
+      selected: true,
+      isLatest: true,
+    },
+    ...stableFirmwareReleases.slice(1).map((x) => {
+      return {
+        ...x,
+        classNames: "bg-meshtastic-green",
+        type: "stable",
+        selected: false,
+        isLatest: false,
+      };
+    }),
+  ];
   return (
     <div className="flow-root">
       <div className="border-b border-gray-200 py-2">
@@ -79,10 +61,10 @@ export default function Releases() {
         </div>
       </div>
       <ul className="overflow-scroll">
-        {releases.map((item, itemIdx) => (
+        {firmwares.map((item, itemIdx) => (
           <li key={item.id}>
             <div className="relative">
-              {itemIdx !== releases.length - 1 ? (
+              {itemIdx !== firmwares.length - 1 ? (
                 <span
                   aria-hidden="true"
                   className="absolute left-8 top-4 -ml-px h-full w-0.5 bg-gray-200"
@@ -94,13 +76,11 @@ export default function Releases() {
                 <div>
                   <span
                     className={classNames(
-                      item.type == "alpha"
-                        ? "bg-orange-500"
-                        : "bg-meshtastic-green",
+                      item.classNames,
                       "flex h-8 w-8 items-center justify-center rounded-full ring-8 ring-white",
                     )}
                   >
-                    <item.icon
+                    <MeshtasticIcon
                       aria-hidden="true"
                       className="h-5 w-5 text-white"
                     />
@@ -108,13 +88,14 @@ export default function Releases() {
                 </div>
                 <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
                   <div>
-                    <p className="text-sm text-gray-500">{item.content}</p>
+                    <p className="text-sm text-gray-500">{item.title}</p>
                     <p className="text-xs italic text-gray-500">
-                      {item.type === "alpha" ? "Pre-release" : "Stable"}
+                      {item.type === "alpha" || item.type === "preview"
+                        ? "Pre-release"
+                        : "Stable"}
                     </p>
                   </div>
                   <div className="flex flex-col whitespace-nowrap text-right text-sm text-gray-500">
-                    <time dateTime={item.datetime}>{item.date}</time>
                     {item.isLatest && (
                       <span className="inline-flex items-center rounded-full bg-meshtastic-green px-2 py-0.5 text-xs font-medium text-gray-900">
                         Latest
