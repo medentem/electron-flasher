50773f128dea84da296dc90b8613bb4838f49dbd
medentem <info@medentem.com>
2024-10-22 19:20:52 -0500

    fixed update

--- src/components/DeviceCard.tsx
+++ src/components/DeviceCard.tsx
@@ -185,7 +185,7 @@ export default function DeviceCard() {
                       Device Serial
                     </dt>
                     <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                      {selectedPort.serialNumber}
+                      {`${"*".repeat(selectedPort.serialNumber.length - 4)}${selectedPort.serialNumber.slice(selectedPort.serialNumber.length - 4)}`}
                     </dd>
                   </div>
                   <div className="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
