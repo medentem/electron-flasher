85cfcb55da74a4caba655eb4f2bcf0a408db7103
medentem <info@medentem.com>
2024-11-19 10:29:09 -0600

    remove force

--- forge.config.ts
+++ forge.config.ts
@@ -26,7 +26,7 @@ const config: ForgeConfig = {
           requirements: undefined,
           strictVerify: false,
           gatekeeperAssess: false,
-          additionalArguments: ["--force", "--verbose"],
+          additionalArguments: ["--verbose"],
         };
       },
     },
