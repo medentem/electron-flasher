f579c2f1cc43279612359c44970596a5576aea72
medentem <info@medentem.com>
2024-12-13 13:47:57 -0600

    core deps done

--- src/components/CustomizeFirmwareDialog.tsx
+++ src/components/CustomizeFirmwareDialog.tsx
@@ -9,7 +9,7 @@ import {
   CheckCircleIcon,
 } from "@heroicons/react/24/solid";
 import MeshtasticIcon from "./MeshtasticIcon";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export interface CustomizeFirmwareDialogProps {
   open: boolean;
@@ -21,6 +21,7 @@ export default function CustomizeFirmwareDialog(
 ) {
   const { open, onClose } = props;
   const [isLoading, setIsLoading] = useState(false);
+  const [allDepsInstalled, setAllDepsInstalled] = useState(false);
   const [pythonInstalled, setPythonInstalled] = useState<undefined | boolean>();
   const [platformIOInstalled, setPlatformIOInstalled] = useState<
     undefined | boolean
@@ -34,7 +35,20 @@ export default function CustomizeFirmwareDialog(
     "Additional Software Required",
   );
 
+  useEffect(() => {
+    setAllDepsInstalled(pythonInstalled && platformIOInstalled);
+    if (pythonInstalled && platformIOInstalled) {
+      setDialogTitle("All Set!");
+    } else {
+      setDialogTitle("Additional Software Required");
+    }
+  }, [pythonInstalled, platformIOInstalled]);
+
   const checkSoftware = () => {
+    if (allDepsInstalled) {
+      //lclOnClose();
+      return;
+    }
     setIsLoading(true);
     setCheckingDependencies(true);
     setDialogTitle("Checking Software...");
@@ -64,6 +78,7 @@ export default function CustomizeFirmwareDialog(
             );
           });
         }
+        setIsLoading(false);
       });
     });
   };
@@ -92,10 +107,16 @@ export default function CustomizeFirmwareDialog(
           >
             <div>
               <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full">
-                {!isLoading && (
+                {!isLoading && !allDepsInstalled && (
                   <ExclamationCircleIcon
                     aria-hidden="true"
                     className="h-12 w-12 text-gray-500 dark:text-gray-400"
+                  />
+                )}
+                {!isLoading && allDepsInstalled && (
+                  <CheckCircleIcon
+                    aria-hidden="true"
+                    className="h-12 w-12 text-meshtastic-green"
                   />
                 )}
                 {isLoading && (
@@ -207,30 +228,6 @@ export default function CustomizeFirmwareDialog(
                           )}
                         </div>
                       </div>
-                      <div className="p-2 flex justify-between">
-                        <div className="text-left">
-                          <h4>Git</h4>
-                          <p className="text-xs text-gray-500">Waiting...</p>
-                        </div>
-                        <div className="h-8 w-8">
-                          <svg
-                            aria-hidden="true"
-                            className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-meshtastic-green absolute"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="currentFill"
-                            />
-                          </svg>
-                        </div>
-                      </div>
                     </div>
                   )}
                   {!checkingDependencies && (
@@ -267,7 +264,11 @@ export default function CustomizeFirmwareDialog(
                 disabled={isLoading}
                 className={`inline-flex w-full justify-center rounded-md ${isLoading ? "bg-gray-300" : "bg-meshtastic-green"} px-3 py-2 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
               >
-                {isLoading ? "Checking..." : "Check Now"}
+                {isLoading
+                  ? "Checking..."
+                  : allDepsInstalled
+                    ? "Continue"
+                    : "Check Now"}
               </button>
             </div>
           </DialogPanel>
